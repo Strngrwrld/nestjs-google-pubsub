5ade14ded39e389b0c22194bea63f6ec24da3f21
Gustavo Jimenez <gjimenez.code@gmail.com>
2022-06-28 08:37:30 -0500

    change pubsub options

--- src/google-pubsub.config.ts
+++ src/google-pubsub.config.ts
@@ -11,14 +11,14 @@ import { PubSubService } from './google-pubsub.service';
 
 export class PubSubConfig implements GooglePubSubOptions {
   constructor(
-    public topic: string,
-    public subscription: string,
-    public replyTopic: string,
-    public replySubscription: string,
-    public noAck: boolean,
-    public publisher: PublishOptions,
-    public subscriber: SubscriberOptions,
     public clientConfig: ClientConfig,
+    public topic?: string,
+    public subscription?: string,
+    public replyTopic?: string,
+    public replySubscription?: string,
+    public noAck?: boolean,
+    public publisher?: PublishOptions,
+    public subscriber?: SubscriberOptions
   ) { }
 }
 
